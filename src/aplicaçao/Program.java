47fcbf383a1009847962ffdb811a11c691249cd2
Guilherme Fogaça <guilhermefogaca2013@gmail.com>
2019-05-25 23:19:42 -0300

    Checkmate logic

--- src/aplicaçao/Program.java
+++ src/aplicaçao/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		List<ChessPiece> captured = new ArrayList<>();
 	
 		UI.printBoard(chessmatch.getpieces());
-		while (true) {
+		while (!chessmatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessmatch, captured);
@@ -51,6 +51,8 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessmatch, captured);
 	}
 
 }
